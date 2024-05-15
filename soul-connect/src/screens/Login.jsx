d2412090da53a349e9355cf74089780bd9f8f7fe
Reza arga <reza.arga29@gmail.com>
2024-05-15 22:00:42 +0700

    create: create register&login screens

---
+++ soul-connect/src/screens/Login.jsx
@@ -0,0 +1,146 @@
+import React, { useState } from "react";
+import {
+  View,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  ScrollView,
+  StyleSheet,
+  Image,
+} from "react-native";
+
+export default function LoginForm({ navigation }) {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleLogin = () => {
+    // Lakukan sesuatu saat tombol login ditekan
+    console.log("Login button pressed!");
+  };
+
+  return (
+    <ScrollView contentContainerStyle={styles.container}>
+      <Text style={styles.title}>Sign In</Text>
+      <View style={styles.inputContainer}>
+        <TextInput
+          style={styles.input}
+          placeholder="Email"
+          value={email}
+          onChangeText={setEmail}
+        />
+      </View>
+      <View style={styles.inputContainer}>
+        <TextInput
+          style={styles.input}
+          placeholder="Password"
+          value={password}
+          onChangeText={setPassword}
+          secureTextEntry
+        />
+      </View>
+      <TouchableOpacity style={styles.button} onPress={handleLogin}>
+        <Text style={styles.buttonText}>SIGN IN</Text>
+      </TouchableOpacity>
+      <View style={styles.dividerContainer}>
+        <View style={styles.dividerLine}></View>
+        <Text style={styles.dividerText}>or</Text>
+        <View style={styles.dividerLine}></View>
+      </View>
+      <TouchableOpacity style={styles.googleButton}>
+        <Image
+          style={styles.googleLogo}
+          source={{
+            uri: "https://e7.pngegg.com/pngimages/56/318/png-clipart-google-logo-logo-logo-company-text.png",
+          }}
+        />
+        <Text style={styles.googleText}>Sign In with Google</Text>
+      </TouchableOpacity>
+      <Text style={styles.registerText}>
+        If you don't have an account, please{" "}
+        <Text
+          style={styles.registerLink}
+          onPress={() => navigation.navigate("Register")}
+        >
+          Sign Up here
+        </Text>
+        .
+      </Text>
+    </ScrollView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flexGrow: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#fff",
+    paddingVertical: 20,
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: "bold",
+    marginBottom: 10,
+  },
+  inputContainer: {
+    width: "80%",
+    marginBottom: 10,
+  },
+  input: {
+    borderBottomWidth: 1,
+    borderBottomColor: "#CCCCCC",
+    paddingVertical: 5,
+  },
+  button: {
+    backgroundColor: "#AA3FEC",
+    borderRadius: 30,
+    paddingVertical: 14,
+    paddingHorizontal: 20,
+    marginTop: 20,
+    width: "80%",
+    alignItems: "center",
+  },
+  buttonText: {
+    color: "white",
+    fontWeight: "bold",
+    fontSize: 16,
+    textAlign: "center",
+  },
+  dividerContainer: {
+    flexDirection: "row",
+    alignItems: "center",
+    marginVertical: 20,
+    width: "80%",
+  },
+  dividerLine: {
+    flex: 1,
+    height: 1,
+    backgroundColor: "#CCCCCC",
+  },
+  dividerText: {
+    marginHorizontal: 10,
+    color: "#6B46C1",
+    fontWeight: "bold",
+    fontSize: 16,
+  },
+  googleButton: {
+    flexDirection: "row",
+    alignItems: "center",
+  },
+  googleLogo: {
+    width: 20,
+    height: 20,
+    marginRight: 10,
+  },
+  googleText: {
+    color: "#4285F4",
+    fontWeight: "bold",
+    fontSize: 16,
+  },
+  registerText: {
+    marginTop: 10,
+  },
+  registerLink: {
+    color: "#4285F4",
+  },
+});
